dfd71e5005d91e7f31c3fbeaf1a74f390118c174
sasanka <sasankasahoo2020@outlook.com>
2023-03-15 01:29:01 +0530

    Update setupRemoteMongo.sh

--- setupRemoteMongo.sh
+++ setupRemoteMongo.sh
@@ -1,5 +1,5 @@
 # Setup file template to upload data to MongoDB Atlas
 # mongoimport --uri mongodb+srv://sasanka1998:ATHSENJAy1522@qkart-node.jqtbe.mongodb.net/myFirstDatabase --collection users --file data/export_qkart_users.json
 # mongoimport --uri mongodb+srv://sasanka1998:ATHSENJAy1522@qkart-node.jqtbe.mongodb.net/myFirstDatabase --collection products --file data/export_qkart_products.json
-mongoimport --uri "mongodb+srv://sasanka1998:ATHSENJAy1522@qkart-node.jqtbe.mongodb.net/qkart?retryWrites=true&w=majority" --drop --collection users --file data/export_qkart_users.json
-mongoimport --uri "mongodb+srv://sasanka1998:ATHSENJAy1522@qkart-node.jqtbe.mongodb.net/qkart?retryWrites=true&w=majority" --drop --collection products --file data/export_qkart_products.json
+mongoimport --uri "mongodb+srv://sasanka_1998:SASANOv1998@qkart.ojwmdjd.mongodb.net/test" --drop --collection users --file data/export_qkart_users.json
+mongoimport --uri "mongodb+srv://sasanka_1998:SASANOv1998@qkart.ojwmdjd.mongodb.net/test" --drop --collection products --file data/export_qkart_products.json
